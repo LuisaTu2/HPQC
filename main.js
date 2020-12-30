3cc588563cf001e6d7abb649347b31884046ad81
Pegoraro, Luisa <luisapegoraro@quickenloans.com>
2020-12-30 02:49:33 -0500

    Added click event response and css

--- main.js
+++ main.js
@@ -8,23 +8,22 @@ $(document).ready(function(){
         {id: 101, firstName: 'Nike',  lastName: 'Dattani',   imgName: 'NikeDattani.png',     title: 'PhD Oxford'},
         {id: 102, firstName: 'Erin',  lastName: 'Zuo',       imgName: 'ErinZuo.jpg',         title: 'PhD WIT'},
         {id: 103, firstName: 'Deyan', lastName: 'Mihaylov',  imgName: 'DeyanMihaylov.png',   title: 'PhD Cambridge'}
-    ]
+    ];
 
     faculty.forEach(person => {
-        let newCard = `
+        let card = `
             <div class="person" id=${person.id}" >
                 <p class="personName"> ${person.firstName} ${person.lastName} </p>
                 <img src="images/${person.imgName}">
                 <p class="personTitle">${person.title}</p>
-                <span style="display:block; margin-top:-27px;"></span>
+                <span></span>
             </div>`      
-        $('#peopleFlexList').append(newCard); 
+        $('#peopleFlexList').append(card); 
     });
     
-    // Need to attach event listener synchronously!!
     faculty.forEach(p => {
-        console.log(p.firstName);
-        $(`#${p.id}`).on("click", showBio);
+        console.log( `#${p.id}`);
+        $('#peopleFlexList').on("click", "div", p.id, showBio); // 'on' is equivalent to deprecated 'delegate' method
     });
 
     document.getElementById('backToPeopleList').addEventListener('click', backToPeopleList);
